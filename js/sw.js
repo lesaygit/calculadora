dbd227d94ed7d5914188cbd6fce6b00be4e9880b
yasel <lesaywd@gmaiul.com>
2024-11-19 15:42:53 -0300

    pwa

--- js/sw.js
+++ js/sw.js
@@ -3,58 +3,57 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/css/styles.css',
-  '/js/calculator.js'
+  '/js/calculator.js',
+  './img/ProgramadorFitness.png',
+  './img/favicon.png'
 ];
 
-// Instalar el Service Worker y cachear recursos
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Cache abierta:', CACHE_NAME);
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Activar el Service Worker y eliminar cachés antiguas
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Eliminando caché antigua:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Interceptar solicitudes y responder desde caché o red
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    // Si es navegación, servir index.html desde la caché
-    event.respondWith(
-      caches.match('/index.html').then((response) => {
-        return response || fetch(event.request);
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
       })
-    );
-  } else {
-    // Si no, buscar en la caché o ir a la red
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request).then((networkResponse) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        });
-      }).catch(() => {
-        // Opcional: Manejar recursos faltantes o páginas de error
-        return new Response('Sin conexión y recurso no disponible.');
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
       })
-    );
-  }
-});
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
+
